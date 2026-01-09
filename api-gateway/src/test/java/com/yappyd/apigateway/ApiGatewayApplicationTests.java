cec72d52124ed9365ab6c64000346a1dacce0cb1
Korobkov Daniil <korobkov200409@gmail.com>
2026-01-09 15:31:08 +0300

    Initialize project structure with API Gateway, Auth Service, and User Service

---
+++ api-gateway/src/test/java/com/yappyd/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yappyd.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
