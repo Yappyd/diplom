fc58e8cdae8c939873bc2362c28f37d39e3d48e1
Korobkov Daniil <korobkov200409@gmail.com>
2026-02-07 16:32:58 +0300

    The user-service init schema has been created

---
+++ user-service/src/main/java/com/yappyd/userservice/model/User.java
@@ -0,0 +1,22 @@
+//package com.yappyd.userservice.model;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+//import lombok.Getter;
+//
+//import java.util.UUID;
+//
+//@Entity
+//@Table(name = "users")
+//@Getter
+//public class User {
+//    @Id
+//    @Column(name = "user_id", nullable = false)
+//    private UUID id;
+//
+//    @Column(name = "phone_number", nullable = false, unique = true, length = 20)
+//    private String phoneNumber;
+//
+//}
