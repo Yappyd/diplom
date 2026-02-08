3a738f53bd0a422779e4bc8c80c0ec09625b17d1
Korobkov Daniil <korobkov200409@gmail.com>
2026-02-08 21:22:39 +0300

    Add RabbitMQ service to Docker

--- user-service/src/main/java/com/yappyd/userservice/dto/responce/PublicUserProfileResponse.java
+++ user-service/src/main/java/com/yappyd/userservice/dto/responce/PublicUserProfileResponse.java
@@ -4,7 +4,7 @@ import java.util.UUID;
 
 public record PublicUserProfileResponse(
         UUID userId,
-        String PhoneNumber,
+        String phoneNumber,
         String tag,
         String firstName,
         String lastName
